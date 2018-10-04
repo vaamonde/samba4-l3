e7fed5b4a14839fdeb54f7cff96c62c4f456abd5
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-10-04 14:46:47 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 16/04/2017
-# Data de atualização: 16/04/2017
-# Versão: 0.1
-# Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
-# Kernel >= 4.4.x
+# Data de atualização: 04/10/2018
+# Versão: 0.2
+# Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
+# Kernel >= 4.15.x
 #
 #Arquivo de configuração de parâmetros
 source parametros.sh
